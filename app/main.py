169fd6d5286e9582b5b86f33fbf04b7e9683864a
t-ohtsuka89 <102138393+t-ohtsuka89@users.noreply.github.com>
2024-09-01 04:34:30 +0000

    feat: Add option to specify port number for opening Gather application

--- app/main.py
+++ app/main.py
@@ -1,13 +1,24 @@
 import argparse
 import subprocess
+import socket
 
 
-def open_gather(gather_path):
+def open_gather(gather_path, port):
     try:
         subprocess.run(["open", "-a", gather_path], check=True)
-        print(f"Gatherを開きました: {gather_path}")
     except subprocess.CalledProcessError:
         print(f"エラー: Gatherを開けませんでした: {gather_path}")
+        exit(1)
+
+
+def get_free_port():
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind(("", 0))
+            return s.getsockname()[1]
+    except Exception as e:
+        print(f"エラー: ポートの取得に失敗しました: {e}")
+        exit(1)
 
 
 def main():
@@ -15,18 +26,24 @@ def main():
         description="複数のファイルパスを処理し、Gatherを開くツール"
     )
     parser.add_argument(
-        "-p",
-        "--path",
+        "-g",
+        "--gather-path",
         default="Gather",
         help="Gatherアプリケーションのパスまたは名前（デフォルト: Gather）",
     )
     parser.add_argument(
         "-s", "--scripts", nargs="+", help="実行するスクリプトのパス", required=False
     )
-
+    parser.add_argument(
+        "-p",
+        "--port",
+        type=int,
+        default=get_free_port(),
+        help="ポート番号（デフォルト: 自動取得）",
+    )
     args = parser.parse_args()
 
-    open_gather(args.path)
+    open_gather(args.gather_path, args.port)
 
     if args.scripts:
         for script_path in args.scripts:
