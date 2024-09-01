b52caa8c36db83364d4b80ad51aa3b4f21882a20
t-ohtsuka89 <102138393+t-ohtsuka89@users.noreply.github.com>
2024-09-01 09:19:58 +0000

    refactor: Remove ScriptExecutorInterface and simplify ScriptExecutor

--- app/script_executor.py
+++ app/script_executor.py
@@ -1,6 +1,5 @@
 import asyncio
 import logging
-from abc import ABC, abstractmethod
 
 from app.chromium_debug_protocol_executor import ChromiumDebugProtocolExecutor
 from app.exceptions import JavaScriptExecutionError
@@ -8,14 +7,8 @@ from app.exceptions import JavaScriptExecutionError
 logger = logging.getLogger(__name__)
 
 
-class ScriptExecutorInterface(ABC):
-    @abstractmethod
-    async def execute_script(self, window, script_path):
-        pass
-
-
 class ScriptExecutor:
-    def __init__(self, window, executor: ScriptExecutorInterface = None):
+    def __init__(self, window, executor=None):
         self.window = window
         self.executor = executor or ChromiumDebugProtocolExecutor()
 
